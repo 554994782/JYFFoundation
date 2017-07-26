71361f20c7f18828db86296638bcd876fdd5481c
jiangyunfeng <554994782@qq.com>
2017-07-26 21:23:18 +0800

    update add framework

---
+++ JYFFoundation/JYFFoundation.h
@@ -0,0 +1,19 @@
+//
+//  JYFFoundation.h
+//  JYFFoundation
+//
+//  Created by jiang on 2017/7/26.
+//  Copyright © 2017年 jiangYunFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYFFoundation.
+FOUNDATION_EXPORT double JYFFoundationVersionNumber;
+
+//! Project version string for JYFFoundation.
+FOUNDATION_EXPORT const unsigned char JYFFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYFFoundation/PublicHeader.h>
+
+
